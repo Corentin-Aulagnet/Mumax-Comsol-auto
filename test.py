c59d4c63c31c64541c7de098f68b99ac98a9e48b
Corentin Aulagnet <corentin.aulagnet@gmail.com>
2023-11-15 13:49:50 +0100

    Unit test - in dev

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import ComsolMumaxPipeline
 
 if __name__=="__main__":
     ##Test mumaxPipeline
-    MumaxPipeline.StartPipeline("tests/MumaxPipeline/config_test.yml",60,10800,test=True)
+    MumaxPipeline.StartPipeline("tests/MumaxPipeline/config_test.yml",60,300,test=True)
     ##Test ComsolMumaxPipeline
     ComsolMumaxPipeline.StartPipeline("tests/ComsolMumaxPipeline/config_test.yml",60,10800,test=True)
     ##Test
